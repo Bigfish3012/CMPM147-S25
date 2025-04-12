15b913ae224e5bdd76195cac45eaa0c58c351394
bigfish3012 <lichengkun0907@gmail.com>
2025-04-12 16:28:50 -0700

    change font for exp2, made wave, clouds, sky color

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,5 +1,5 @@
 // sketch.js - purpose and description here
-// Author: Your Name
+// Author: Chengkun Li
 // Date:
 
 // Here is how you might set up an OOP p5.js project
@@ -53,27 +53,63 @@ function setup() {
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
+  randomSeed(100);
   background(220);    
   // call a method on the instance
   myInstance.myMethod();
 
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
+  //sky gradient
+  for (let x = 0; x < width; x++) {
+    let inter = map(x, 0, width, 0, 1); // 0 on the left, 1 on the right
+    let c = lerpColor(color("#EEF5FF"), color("#578FCA"), inter); // Blend the two colors
+    stroke(c);
+    line(x, 0, x, height); // Draw vertical lines to fill canvas
+  }
 
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  // Ocean
+  drawwaves();
+  let wave_height = 350;
+  for(let i = 0; i < 30; i++) {
+    drawwaves(centerVert + wave_height);
+    wave_height += 20; // Increase height for each wave
+  }
+
+  // Draw clouds
+  const clouds = random(5, 7); // Random number of clouds
+  const scrub = mouseX / width;
+  for (let i = 0; i < clouds; i++) {
+    let x = width * ((random() + (scrub / 50 + millis() / 500000.0)) % 1);
+    let y = height / 4 + random(-50, 50); // Height with a bit of vertical variance
+  
+    drawCloud(x, y);
+  }
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
     // code to run when mouse is pressed
+}
+
+function drawCloud(xPos, yPos) {
+  noStroke();
+  fill("#FBFBFB");
+  ellipse(xPos, yPos, 90, 50); // Main body of the cloud
+  ellipse(xPos + random(40, 70), yPos, 90, 50);
+  ellipse(xPos + random(50, 70), yPos - random(20, 30), 90, 50);
+}
+
+function drawwaves(height = canvasContainer.height()) {
+  let OceanColors = ["#2A629A", "#578FCA", "#2A629A", "#0F67B1", "#7AA2E3", "#008DDA"];
+  noStroke();
+  fill(OceanColors[int(random(0, 5))]); 
+  beginShape();
+  vertex(0, height);
+  const steps = 20;
+  for (let i = 0; i < steps + 1; i++) {
+    let x = (width * i) / steps;
+    let y = height / 2 - (random() * random() * height) / 40 - height / 100;
+    vertex(x, y);
+  }
+  vertex(width, height);
+  endShape(CLOSE);
 }
